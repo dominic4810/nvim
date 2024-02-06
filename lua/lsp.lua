551409591190a388cc6fb3dcc09fe11122bf7993
Dominic Wong <dominicwong999@gmail.com>
2024-02-06 23:16:20 +0100

    Extra keybindings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -8,6 +8,8 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>dd', vim.diagnostic.disable, opts)
+vim.keymap.set('n', '<leader>dD', vim.diagnostic.enable, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
