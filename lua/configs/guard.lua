06ef8cfff09d98098677d79c131dc2a806f982c2
Dominic Wong <dominicwong999@gmail.com>
2024-10-23 10:24:33 +0200

    update

--- lua/configs/guard.lua
+++ lua/configs/guard.lua
@@ -24,9 +24,10 @@ ft('json'):fmt({
 })
 
 -- Call setup() LAST!
-require('guard').setup({
+local vim = vim
+vim.g.guard_config = {
     -- the only options for the setup function
     fmt_on_save = false,
     -- Use lsp if no formatter was defined for this filetype
     lsp_as_default_formatter = false,
-})
+}
