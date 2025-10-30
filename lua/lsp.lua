6932ae6ece64d6b71206629911a6d42d83b9d68c
Dominic Wong <dominicwong999@gmail.com>
2025-10-30 17:27:46 +0100

    fold

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -46,6 +46,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.cmd("normal! zx")
   end
 })
 
